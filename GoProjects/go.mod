8b11d9d64872746fc3470d20f0383187e280d8dc
Ešref Pivčić <esref.pivcic@edu.fit.ba>
2023-10-31 21:03:41 +0100

    Add image handling - test.go

--- GoProjects/go.mod
+++ GoProjects/go.mod
@@ -6,6 +6,8 @@ require github.com/lib/pq v1.10.9
 
 require github.com/gorilla/mux v1.8.0
 
+require github.com/google/uuid v1.4.0 
+
 require (
 	github.com/felixge/httpsnoop v1.0.1 //indirect
 	github.com/gorilla/handlers v1.5.1
--- React-App/src/App.js
+++ React-App/src/App.js
@@ -37,118 +37,152 @@ const App = () => {
 
 export default App;
 */
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
-const buttonStyle = {
-  background: '#333',
-  color: '#fff',
-  padding: '20px',
-  margin: '10px',
-  cursor: 'pointer',
-  border: 'none',
-  borderRadius: '10px',
-  width: '300px',
-  height: '300px',
-};
+/*import React, { useState } from 'react';
 
-function App() {
-  const bodyStyle = {
-    background: '#111',
-    color: '#fff',
-    margin: 0,
-    padding: 0,
+function TestUploadForm() {
+  const [test, setTest] = useState({
+    Title: '',
+    Description: '',
+    Questions: [],
+    Category: '',
+    ImageBase64: '',
+    Duration: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'Duration') {
+      const duration = parseInt(value, 10);
+      setTest({ ...test, [name]: duration });
+    } else if (name === 'Questions') {
+      const questionsArray = value.split(',').map((str) => parseInt(str.trim(), 10));
+      setTest({ ...test, [name]: questionsArray });
+    } else {
+      setTest({ ...test, [name]: value });
+    }
   };
 
-  const appStyle = {
-    textAlign: 'center',
-    padding: '20px',
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const base64Image = e.target.result.split(',')[1];
+      setTest({ ...test, ImageBase64: base64Image });
+    };
+
+    reader.readAsDataURL(file);
+    setTest({ ...test, ImageName: file.name });
   };
 
-  const titleStyle = {
-    color: '#61dafb',
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const categoryData = {
-    a: {
-      headline: 'A Category',
-      description: 'A and A1 category (motorcycle and moped)',
-      photo: process.env.PUBLIC_URL + '/images/motor.png',
-    },
-    b: {
-      headline: 'B Category',
-      description: 'B and B1 category (car and moped)',
-      photo: process.env.PUBLIC_URL + '/images/taxi.png',
-    },
-    c: {
-      headline: 'C Category',
-      description: 'C and C1 category (truck and truck 7.5t)',
-      photo: process.env.PUBLIC_URL + '/images/truck.png',
-    },
-    d: {
-      headline: 'D Category',
-      description: 'D category (bus)',
-      photo: process.env.PUBLIC_URL + '/images/bus.png',
-    },
-    t: {
-      headline: 'T Category',
-      description: 'T category (tractor, working machines)',
-      photo: process.env.PUBLIC_URL + '/images/tractor.png',
-    },
-    firstAid: {
-      headline: 'First Aid',
-      description: 'First aid - the most important test',
-      photo: process.env.PUBLIC_URL + '/images/ambulance.png',
-    },
+    try {
+      const response = await fetch('http://localhost:8080/test/post', {
+        method: 'POST',
+        body: JSON.stringify(test),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+      } else {
+      }
+    } catch (error) {
+    }
   };
 
   return (
-    <BrowserRouter>
-      <div className="app" style={bodyStyle}>
-        <div style={appStyle}>
-          <h1 className="app-title" style={titleStyle}>
-            eDrivingSchool
-          </h1>
-          <Routes>
-            <Route path="/" element={<CategoryButtons categoryData={categoryData} />} />
-            {Object.keys(categoryData).map((category) => (
-              <Route
-                key={category}
-                path={`/${category.toLowerCase()}test`}
-                element={<CategoryPage category={category} />}
-              />
-            ))}
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input type="text" name="Title" value={test.Title} onChange={handleInputChange} />
+      </label>
+      <label>
+        Description:
+        <input type="text" name="Description" value={test.Description} onChange={handleInputChange} />
+      </label>
+      <label>
+        Category:
+        <input type="text" name="Category" value={test.Category} onChange={handleInputChange} />
+      </label>
+      <label>
+        Duration:
+        <input type="number" name="Duration" value={test.Duration} onChange={handleInputChange} />
+      </label>
+      <label>
+        Questions (comma-separated):
+        <textarea name="Questions" value={test.Questions} onChange={handleInputChange} />
+      </label>
+      <label>
+        Image:
+        <input type="file" name="ImageBase64" onChange={handleImageUpload} />
+      </label>
+      <button type="submit">Upload Test</button>
+    </form>
   );
 }
 
-function CategoryButtons({ categoryData }) {
+export default TestUploadForm;
+*/
+
+import React, { useState, useEffect } from "react";
+
+function TestButton() {
+  const [testData, setTestData] = useState([]);
+
+  useEffect(() => {
+    fetchTestData();
+  }, []);
+
+  const razmakni = {
+    margin: '20px',
+    padding: '20px'
+  };
+
+  const fetchTestData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/tests/get");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setTestData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <div className="categories">
-      {Object.keys(categoryData).map((category) => (
-        <Link key={category} to={`/${category.toLowerCase()}test`} style={{ textDecoration: 'none' }}>
-          <button style={buttonStyle}>
-            <img
-              src={categoryData[category].photo}
-              alt={categoryData[category].headline}
-              style={{ maxWidth: '100%', maxHeight: '100%', marginBottom: '10px' }}
-            />
-            <h2>{categoryData[category].headline}</h2>
-            <p>{categoryData[category].description}</p>
-          </button>
-        </Link>
-      ))}
+    <div>
+      <h2>Test Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th style={razmakni}>Title</th>
+            <th style={razmakni}>Description</th>
+            <th style={razmakni}>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {testData.map((test) => (
+            <tr key={test.ID}>
+              <td style={razmakni}>{test.Title}</td>
+              <td style={razmakni}>{test.Description}</td>
+              <td style={razmakni}>
+                {test.ImageBase64 && (
+                  <img src={`data:image/png;base64,${test.ImageBase64}`} alt={`Test: ${test.Title}`} width="150" />
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-function CategoryPage({ category }) {
-  return <p>This is the {category} category page</p>;
-}
-
-export default App;
-
+export default TestButton;
 
