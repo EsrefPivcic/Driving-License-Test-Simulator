5dadb6a29066bfc88342bdefc8a196f68b50bf8a
Ešref Pivčić <esref.pivcic@edu.fit.ba>
2023-11-29 13:53:30 +0100

    Add navbar, Update authentication

--- React-App/src/components/App.js
+++ React-App/src/components/App.js
@@ -11,14 +11,12 @@ import UserProfilePage from "./UserProfilePage/UserProfilePage";
 import { useAuth } from "./AuthContext/AuthContext";
 import ExamHistoryPage from "./ExamHistoryPage/ExamHistoryPage";
 import AddOptionPage from "./AddOptionPage/AddOptionPage";
-import AdminHomePage from "./AdminHomePage/AdminHomePage";
 import "./App.css";
 
 function App() {
-  const { isAuthenticated, clearAuthStatus, ValidateToken, login, closeLogin, fetchUserData, userData } = useAuth();
+  const { isAuthenticated, clearAuthStatus, ValidateToken, login, closeLogin, fetchUserData, userData, LogOut } = useAuth();
   const [message, setMessage] = useState('');
   const [admin, setAdmin] = useState(false);
-  const [student, setStudent] = useState(false);
 
   useEffect(() => {
     const checkToken = async () => {
@@ -27,7 +25,9 @@ function App() {
         if (!isValid) {
           clearAuthStatus();
         }
-        fetchUserData();
+        else {
+          fetchUserData();
+        }
       }
     };
     checkToken();
@@ -46,50 +46,80 @@ function App() {
 
   useEffect(() => {
     setAdmin(userData.IsAdmin);
-    setStudent(userData.IsStudent);
   }, [userData]);
 
-    return (
-      <BrowserRouter>
-        <div className="app">
-          <div className="app-content">
-            <h1 className="app-title">
-              <Link to="/">eDrivingSchool</Link>
-            </h1>
-            {message && <p className="message">{message}</p>}
-            {isAuthenticated ? (
-              <Routes>
-                {admin && (
-                  <>
-                    <Route path="addtest" element={<AddTestPage />} />
-                    <Route path="addquestion" element={<AddQuestionPage />} />
-                    <Route path="addoption" element={<AddOptionPage />} />
-                    <Route path="/" element={<AdminHomePage />} />
-                  </>
+  const HandleLogOut = async () => {
+    LogOut();
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <div className="app-content">
+          <h1 className="app-title">
+            <Link to="/">eDrivingSchool</Link>
+          </h1>
+          <div className="navbar">
+            {isAuthenticated && (
+              <div className="profile-dropdown">
+                {userData.ImageBase64 && (
+                  <img
+                    src={`data:image/png;base64,${userData.ImageBase64}`}
+                    alt={`User: ${userData.Username}`}
+                    className="profile-image"
+                  />
                 )}
-                {student && (
-                  <>
-                    <Route path="/" element={<HomePage />} />
-                  </>
-                )}
-                <Route path="examhistory" element={<ExamHistoryPage />} />
-                <Route path="testresults" element={<TestResultsPage />} />
-                <Route path="test" element={<TestPage />} />
-                <Route path="userprofile" element={<UserProfilePage />} />
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
-            ) : (
-              <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="register" element={<RegistrationPage />} />
-                <Route path="*" element={<Navigate to="/" />} />
-              </Routes>
+                {!userData.ImageBase64 && (<img src="images/userimage.jpg" alt={`User: ${userData.Username}`} className="profile-image" />)}
+                <div className="dropdown-content">
+                  <Link to={`/userprofile`} style={{ textDecoration: "none" }}>
+                    <p>User Profile</p>
+                  </Link>
+                  <Link to={`/examhistory`} style={{ textDecoration: "none" }}>
+                    <p>Exam History</p>
+                  </Link>
+                  {admin && (<div><Link to={`/addtest`} style={{ textDecoration: "none" }}>
+                    <p>Add Test</p>
+                  </Link>
+                    <Link to={`/addquestion`} style={{ textDecoration: "none" }}>
+                      <p>Add Question</p>
+                    </Link>
+                    <Link to={`/addoption`} style={{ textDecoration: "none" }}>
+                      <p>Add Option</p>
+                    </Link></div>)}
+                  <p onClick={HandleLogOut}>Logout</p>
+                </div>
+              </div>
             )}
           </div>
+          {message && <p className="message">{message}</p>}
+          {isAuthenticated ? (
+            <Routes>
+              {admin && (
+                <>
+                  <Route path="addtest" element={<AddTestPage />} />
+                  <Route path="addquestion" element={<AddQuestionPage />} />
+                  <Route path="addoption" element={<AddOptionPage />} />
+                </>
+              )}
+              <Route path="/" element={<HomePage />} />
+              <Route path="examhistory" element={<ExamHistoryPage />} />
+              <Route path="testresults" element={<TestResultsPage />} />
+              <Route path="test" element={<TestPage />} />
+              <Route path="userprofile" element={<UserProfilePage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="register" element={<RegistrationPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          )}
         </div>
-      </BrowserRouter>
-    );
-  }
+      </div>
+    </BrowserRouter>
+  );
+}
 
 
 export default App;
